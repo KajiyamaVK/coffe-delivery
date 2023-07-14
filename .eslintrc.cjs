de2cced4b4417b87e5bdb1dbb1eb4be0d7466d92
KajiyamaVK <victor.kajiyama@gmail.com>
2023-07-14 17:17:40 -0300

    chore:First push

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: '@rocketseat/eslint-config/react',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['react'],
+  rules: {},
+}
