9045886ee6fbbeb9b8cf3a6469a504dd1ab3ac87
KajiyamaVK <victor.kajiyama@gmail.com>
2023-07-15 05:00:07 -0300

    style:Creates Header styling

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import { styled } from 'styled-components'
+
+export const Main = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem 160px;
+  width: 100%;
+  background-color: pink;
+`
+
+export const Logo = styled.img`
+  width: 5.3125rem;
+  height: 2.5rem;
+`
